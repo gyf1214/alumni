06c023e5323001db9494f5fe40c1f06f8e9a0b83
gyf1214 <gyf_1214@163.com>
2015-07-20 19:32:35 +0800

    finsh dump

---
+++ util/dump_data.rb
@@ -0,0 +1,19 @@
+require './bootstrap'
+require 'fileutils'
+
+include Alumni
+Manager.files.each do |k, v|
+	Logger.info "Dump files for #{k}, #{v.size} versions"
+	dir = File.expand_path k, Manager::Dump
+	Dir.mkdir dir unless File.exist? dir
+
+	v.each_with_index do |name, index|
+		path = File.expand_path name, Manager::Files
+		dest = File.expand_path "#{index}.jpg", dir
+		if File.exist? path
+			FileUtils.cp path, dest
+		else
+			Logger.warn "Skip #{name} version: #{index}. Source not found!"
+		end
+	end
+end
